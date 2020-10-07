30e49343c83dc072ceae9e34ec25372326c308af
Diogo Verde <diogo.verde05@gmail.com>
2020-10-07 17:21:35 +0100

    added components for rating, homescreen and products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 };
