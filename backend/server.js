b0f22ca92b6e13e541476ff1b79222a7d9d0b9ac
Diogo Verde <diogo.verde05@gmail.com>
2020-11-12 14:34:53 +0000

    added place order backend controller and route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
 
 app.use(notFound);
