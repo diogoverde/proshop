6893e0919d718af2cd489a9b2f364d1019912ac3
Diogo Verde <diogo.verde05@gmail.com>
2020-11-17 18:01:27 +0000

    Front End Image Upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,12 +1,11 @@
 import path from "path";
 import express from "express";
 import multer from "multer";
-
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "/uploads");
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
     cb(
@@ -17,14 +16,14 @@ const storage = multer.diskStorage({
 });
 
 function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeq|png/;
+  const filetypes = /jpg|jpeg|png/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb("Images only please.");
+    cb("Images only!");
   }
 }
 
